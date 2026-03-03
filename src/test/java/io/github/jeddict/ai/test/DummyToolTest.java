0f8287f76c9c249d8be7b0204f14f1f70db2929f
Stefano Fornari <stefano.fornari@gmail.com>
2026-03-03 08:33:19 +0100

    Improved DummyTool and DummyChatModel with some enhancements and needs to toolify LLMs

--- src/test/java/io/github/jeddict/ai/test/DummyToolTest.java
+++ src/test/java/io/github/jeddict/ai/test/DummyToolTest.java
@@ -19,6 +19,7 @@ package io.github.jeddict.ai.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import static org.assertj.core.api.BDDAssertions.then;
 import org.junit.jupiter.api.Test;
 
@@ -41,4 +42,42 @@ public class DummyToolTest {
         then(t.executed).isTrue(); then(t.executed()).isTrue();
     }
 
+    @Test
+    public void track_execution_with_args() throws IOException {
+        final DummyTool t = new DummyTool();
+        final List<String> arg2 = List.of("val2");
+        final List<String> arg22 = List.of("val22");
+
+        t.dummyToolWithArgs("val1", arg2);
+        then(t.arguments).containsExactly("val1", arg2);
+
+        t.dummyToolWithArgs("val2", arg22);
+        then(t.arguments).containsExactly("val2", arg22);
+    }
+
+    @Test
+    public void return_values() throws IOException {
+        final DummyTool t = new DummyTool();
+        final String arg11 = "val11", arg12 = "val12";
+        final List<String> arg21 = List.of("val21");
+        final List<String> arg22 = List.of("val22");
+
+        then(t.dummyTool()).isEqualTo("true");
+        then(t.dummyToolWithArgs(arg11, arg21)).isEqualTo("true\narg1: val11\narg2: [val21]");
+        then(t.dummyToolWithArgs(arg12, arg22)).isEqualTo("true\narg1: val12\narg2: [val22]");
+    }
+
+    @Test
+    public void reset_cleans_the_status() throws IOException {
+        final DummyTool t = new DummyTool();
+        t.dummyToolWithArgs("val1", List.of());
+
+        then(t.executed).isTrue();
+        then(t.arguments()).isNotNull();
+
+        t.reset();
+
+        then(t.executed).isFalse();
+        then(t.arguments).isNull();
+    }
 }
